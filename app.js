b19ddfebb4a16a604db788b2964f592b680a8aee
Victor Dias Teixeira <vyctorbh@gmail.com>
2015-07-18 12:55:51 +0000

    Init

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var express = require('express'),
 
 // This is needed if the app is run on heroku:
 
-var port = process.env.PORT || 8004;
+var port = process.env.PORT || 80;
 
 // Initialize a new socket.io object. It is bound to 
 // the express app, which allows them to coexist.
